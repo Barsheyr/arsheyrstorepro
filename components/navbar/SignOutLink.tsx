269c7a12006a9fda8b8002ca346a5a889cc2c073
Bashir Amokomowo <barsheyr@gmail.com>
2024-08-07 23:39:37 +0100

    new pushes

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+"use client";
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+import Link from "next/link";
 
 function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: "Logging Out..." });
+  };
+  return (
+    <SignOutButton>
+      <Link href="/" className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 }
-
 export default SignOutLink;
