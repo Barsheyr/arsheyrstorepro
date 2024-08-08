9bf6e2cce7dd2c9de4f71ee36471de0f03214de4
Bashir Amokomowo <barsheyr@gmail.com>
2024-08-08 22:31:00 +0100

    add auth

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { LuUser2 } from "react-icons/lu";
+import { currentUser } from "@clerk/nextjs/server";
 
-function UserIcon() {
-  return <div>UserIcon</div>;
+async function UserIcon() {
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+  if (profileImage)
+    return (
+      <>
+        <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
+      </>
+    );
+  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
-
 export default UserIcon;
