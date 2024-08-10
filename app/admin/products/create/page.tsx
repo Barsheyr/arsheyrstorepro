2f9706952abd01553b0bb10b7553f6c6a5f3bc59
Bashir Amokomowo <barsheyr@gmail.com>
2024-08-10 16:12:17 +0100

    new push

---
+++ app/admin/products/create/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateProductPage() {
+  return <div>CreateProductPage</div>;
+}
+
+export default CreateProductPage;
---
+++ app/admin/products/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductsPage() {
+  return <div> Admin Products Page </div>;
+}
+
+export default ProductsPage;
