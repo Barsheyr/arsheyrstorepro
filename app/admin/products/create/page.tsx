5a7814f3f270767eeca8ee59b70d66147b4e740f
Bashir Amokomowo <barsheyr@gmail.com>
2024-08-11 14:54:05 +0100

    new push

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { faker } from "@faker-js/faker";
+
+const createProductAction = async (formData: FormData) => {
+  "use server";
+  const name = formData.get("name") as string;
+  console.log(name);
+};
 
 function CreateProductPage() {
-  return <div>CreateProductPage</div>;
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">create product</h1>
+      <div className="border p-8 rounded-md">
+        <form action={createProductAction}>
+          <div className="mb-2">
+            <Label htmlFor="name">Product Name</Label>
+            <Input id="name" name="name" type="text" />
+          </div>
+          <Button type="submit" size="lg">
+            Submit
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default CreateProductPage;
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
+  // console.log(auth().userId);
+
+  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
+
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
   if (!isPublicRoute(req)) auth().protect();
 });
 
