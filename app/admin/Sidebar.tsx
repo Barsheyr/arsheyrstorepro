2123fd922099f40d90fabb7ee7e2ab498664ab34
Bashir Amokomowo <barsheyr@gmail.com>
2024-09-23 12:05:23 +0100

    fix

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -18,10 +18,9 @@ function Sidebar() {
             asChild
             className="w-full mb-2 capitalize font-normal justify-start"
             variant={variant}
+            key={link.href}
           >
-            <Link key={link.href} href={link.href}>
-              {link.label}
-            </Link>
+            <Link href={link.href}>{link.label}</Link>
           </Button>
         );
       })}
